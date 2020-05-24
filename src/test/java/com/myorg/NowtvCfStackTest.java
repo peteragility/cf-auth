3f16edccd745f289093196a8cbdd3cb89120cd5b
peterone928 <31908564+peterone928@users.noreply.github.com>
2020-05-24 16:35:06 +0800

    comment out unit test code

--- src/test/java/com/myorg/NowtvCfStackTest.java
+++ src/test/java/com/myorg/NowtvCfStackTest.java
@@ -1,18 +1,9 @@
 package com.myorg;
-import software.amazon.awscdk.core.App;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import org.junit.Test;
-import org.hamcrest.CoreMatchers;
 
 import java.io.IOException;
-import static org.junit.Assert.assertThat;
 
 public class NowtvCfStackTest {
-    private final static ObjectMapper JSON =
-        new ObjectMapper().configure(SerializationFeature.INDENT_OUTPUT, true);
-
     @Test
     public void testStack() throws IOException {
         //App app = new App();
